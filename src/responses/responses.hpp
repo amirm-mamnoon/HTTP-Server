903a84962372274e434b5a1e8c463be7aa2a6b38
StupidUsernamee <amirm.mamnoon@gmail.com>
2026-07-01 02:26:29 +0330

    early response functionallity implemented, more to be done.

---
+++ src/responses/responses.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+struct responseHeader 
+{
+    std::string key;
+    std::string value;
+};
+
+
+struct response
+{
+    std::string httpVersion;
+    unsigned statusCode;
+    std::string statusText;
+    std::vector<responseHeader> headers;
+    std::string body;
+
+    response() : httpVersion("HTTP/1.1"), statusCode(200), statusText("OK") {}
+};
+
+bool sendResponse(int fd, const response& resp);
+bool createResponse(response& resp);
