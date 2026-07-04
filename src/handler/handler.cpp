c4f0aba110a5cab0ea48247dd2d2cc2a84bc3ac3
amirm-mamnoon <amirm.mamnoon@gmail.com>
2026-07-04 23:29:07 +0330

    file handers added to handlers

--- src/handler/handler.cpp
+++ src/handler/handler.cpp
@@ -1,5 +1,9 @@
 #include "handler.hpp"
 
+#include "file_handler.hpp"
+
+std::string file_missing_err_msg = "<h1>500 - Server Configuration Error</h1><p>Missing index.html</p>";
+
 response handle_homepage(request /* req */)
 {
     response resp;
@@ -27,5 +31,29 @@ response handle_404(request /* req */)
     resp.statusText = "Not Found";
     resp.body = "<h1>404 - Page Not Found</h1>";
     resp.headers.push_back({"Content-Type", "text/html"});
+    return resp;
+}
+
+response handle_html_file(request req)
+{
+    response resp;
+
+    auto file_content = read_from_file(req);
+
+    if (file_content.has_value())
+    {
+        resp.statusCode = 200;
+        resp.statusText = "OK";
+        resp.body = file_content.value();
+        resp.headers.push_back({"Content-Type", "text/html"});
+    }
+    else
+    {
+        resp.statusCode = 500;
+        resp.statusText = "Internal Server Error";
+        resp.body = file_missing_err_msg;
+        resp.headers.push_back({"Content-Type", "text/html"});
+    }
+
     return resp;
 }
--- src/handler/handler.hpp
+++ src/handler/handler.hpp
@@ -6,3 +6,4 @@
 response handle_homepage(request req);
 response handle_api_data(request req);
 response handle_404(request req);
+response handle_html_file(request req);
